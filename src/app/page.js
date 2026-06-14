7c6174844b477a188f56c71a95713dd6128bf1eb
sba5022 <sumaiaofficial02@gmail.com>
2026-06-14 22:18:46 +0600

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import Banner from "@/components/Banner";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+  <Banner/>
+    </div>
+  );
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+import { CgProfile } from 'react-icons/cg';
+
+const Navbar = () => {
+    return (
+        <nav className='w-full flex items-center justify-between p-4 bg-white shadow-md'>
+          <ul className='flex items-center gap-4'>
+            <li><Link href="/">Home</Link></li>
+            <li><Link href='/add-destination'>Destinations</Link></li>
+            <li><Link href='/my bookings'>My Bookings</Link></li>
+            <li><Link href='/admin'>Admin</Link></li>
+            
+          </ul>
+          <div>
+            <Image  src={'/assets/Wanderlast.png'} width={200} height={200} alt="Logo"/>
+          </div>
+          <ul className='flex items-center gap-4'>
+            <li className='flex justify-between gap-2'><Link href="/profile"><CgProfile />Profile</Link></li>
+            <li><Link href='/login'>Login</Link></li>
+            <li><Link href='/signup'>Sign Up</Link></li>
+            
+            
+          </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
